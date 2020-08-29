b02d2acb0fea3cf6c07d12dfdeb7dc062ae044bb
chairul <chairulharun@gmail.com>
2020-08-29 16:40:55 +0700

    update kos 29 Agustus 2020

--- app/src/main/java/com/gorontalo/chair/newsidapp/LoginActivity.java
+++ app/src/main/java/com/gorontalo/chair/newsidapp/LoginActivity.java
@@ -8,6 +8,7 @@ import android.app.PendingIntent;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
@@ -15,6 +16,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -72,6 +74,9 @@ public class LoginActivity extends AppCompatActivity {
     private static final String TAG_SUCCESS = "success";
     private static final String TAG_HASIL = "hasil";
 
+    private RelativeLayout relativeLayout;
+    private AnimationDrawable animationDrawable;
+
     private SessionAdapter sessionAdapter;
     private KoneksiAdapter koneksiAdapter;
 
@@ -92,6 +97,11 @@ public class LoginActivity extends AppCompatActivity {
         koneksiAdapter= new KoneksiAdapter(getApplicationContext());
         sessionAdapter = new SessionAdapter(getApplicationContext());
         sessionAdapter.checkLogin();
+
+        relativeLayout = (RelativeLayout) findViewById(R.id.relativelayout);
+        animationDrawable = (AnimationDrawable) relativeLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(5000);
+        animationDrawable.setExitFadeDuration(2000);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -152,6 +162,22 @@ public class LoginActivity extends AppCompatActivity {
                         .check();
             }
         });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (animationDrawable != null && animationDrawable.isRunning()){
+            animationDrawable.stop();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (animationDrawable != null && !animationDrawable.isRunning()){
+            animationDrawable.start();
+        }
     }
 
     private void getLogin(final String ktp, final String token) {
