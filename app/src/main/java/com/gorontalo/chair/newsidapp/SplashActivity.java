e996e770579b9e876f7101fa91083c199db7c75f
chairul <chairulharun@gmail.com>
2020-09-17 14:12:02 +0700

    update kos 17 september 2020

--- app/src/main/java/com/gorontalo/chair/newsidapp/SplashActivity.java
+++ app/src/main/java/com/gorontalo/chair/newsidapp/SplashActivity.java
@@ -3,15 +3,23 @@ package com.gorontalo.chair.newsidapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.RelativeLayout;
 
 public class SplashActivity extends AppCompatActivity {
+    private RelativeLayout relativeLayout;
+    private AnimationDrawable animationDrawable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        animationDrawable = (AnimationDrawable) relativeLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(5000);
+        animationDrawable.setExitFadeDuration(2000);
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -27,5 +35,21 @@ public class SplashActivity extends AppCompatActivity {
                 // TODO Auto-generated method stub
             }
         }, 2000);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (animationDrawable != null && animationDrawable.isRunning()){
+            animationDrawable.stop();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (animationDrawable != null && !animationDrawable.isRunning()){
+            animationDrawable.start();
+        }
     }
 }
